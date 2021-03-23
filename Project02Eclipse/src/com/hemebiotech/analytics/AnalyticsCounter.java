5507f5f41d3f3ce2d51f5f6b256825f84127d71d
Jérôme Guéguin <jerome.gueguin@gmail.com>
2021-03-23 07:26:55 +0100

    Rework of ReadSymptomDataFromFile into ReadSymptomDataFromFile2.java and also ISymptomReader into ISymptomReader2.java. -> Map is used in place of ArrayList which doesn't permit a pair {clue - value }. Adding code for an output to an external file. Javadoc comments added.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -19,7 +19,7 @@ public class AnalyticsCounter {
 	public static void main(String args[]) throws IOException {
 
 		// ->>> Object creation with Call of ReadSymptomDataFromFile2  with file name and path as parameters
-		ReadSymptomDataFromFile file = new ReadSymptomDataFromFile(path + nameFile);
+		ReadSymptomDataFromFile2 file = new ReadSymptomDataFromFile2(path + nameFile);
 
 		// ->> output of the results
 		System.out.println(file.GetSymptoms());
--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader2.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader2.java
@@ -1,20 +1,27 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Anything that will read symptom data from a source
  * The important part is, the return value from the operation, which is a list of strings,
  * that may contain many duplications
- * 
+ *
  * The implementation does not need to order the list
- * 
+ *
+ * @author jgueguin
+ * @version 1.1
  */
+
 public interface ISymptomReader2 {
 	/**
-	 * If no data is available, return an empty List
-	 * 
+	 * If no data is available, return an empty "Map"
+	 *
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	List<String> GetSymptoms ();
+	Map<String,Integer> GetSymptoms () throws IOException;
 }
+
+
