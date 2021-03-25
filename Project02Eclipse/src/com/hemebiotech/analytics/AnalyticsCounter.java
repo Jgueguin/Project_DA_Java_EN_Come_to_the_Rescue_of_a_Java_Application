a30473a5d829d7fe4e54dd69f1f4c8083733301f
Jérôme Guéguin <jerome.gueguin@gmail.com>
2021-03-25 09:29:18 +0100

    Final code V1.0 with javadoc comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,7 +3,7 @@ package com.hemebiotech.analytics;
 import java.io.IOException;
 
 /**
- * Main program to read a txt file and to sort the numbers of symptoms
+ * Main program to read a txt file and to print the numbers of symptoms in a file and on console
  *
  * @author jgueguin
  * @version 1.1
@@ -16,11 +16,10 @@ public class AnalyticsCounter {
 
 	public static void main(String args[]) throws IOException {
 
-		//		ReadSymptomDataFromFile file = new ReadSymptomDataFromFile(null);
 		ReadSymptomDataFromFile file = new ReadSymptomDataFromFile(pathNameFile);
 
-		// ->> print results in 'results.out' file and on console
 		PrintSymptomDataToFile output = new PrintSymptomDataToFile(file.GetSymptoms());
+
 		output.printFile();
 
 
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -6,55 +6,60 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * The goal of this class is to read the contain of a file and to sort it
+ * @author jgueguin
+ * @version 1.1
+ *
+ * @see com.hemebiotech.analytics.ISymptomReader
+ */
+
 
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private String filepath;
+    private String filepath;
 
-	/**
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile(String filepath) {
-		this.filepath = filepath;
-	}
+    /**
+     * @param filepath a full or partial path to file with symptom strings in it, one per line
+     */
+    public ReadSymptomDataFromFile(String filepath) {
+        this.filepath = filepath;
+    }
 
-	/**
-	 * @return
-	 */
+    /**
+     * @return
+     */
 
-	@Override
-	public Map<String, Integer> GetSymptoms() throws IOException {
+    @Override
+    public Map<String, Integer> GetSymptoms() throws IOException {
 
-		Map<String, Integer> mapSymptoms = new TreeMap<>(); // collection to save symptoms and associated occurences
+        Map<String, Integer> mapSymptoms = new TreeMap<>(); // collection to save symptoms and associated occurences
 
-			try {
-				BufferedReader reader = new BufferedReader(new FileReader(filepath));
-				String line = reader.readLine();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filepath));
+            String line = reader.readLine();
 
-				while (line != null) {
+            while (line != null) {
 
-					if (mapSymptoms.containsKey(line)) {
+                if (mapSymptoms.containsKey(line)) {
 
-						int nbrePresent = mapSymptoms.getOrDefault(line, 0);
-						mapSymptoms.replace(line, nbrePresent + 1);
+                    int nbrePresent = mapSymptoms.getOrDefault(line, 0);
+                    mapSymptoms.replace(line, nbrePresent + 1);
 
-					} else {
+                } else {
 
-						mapSymptoms.put(line, 1);
+                    mapSymptoms.put(line, 1);
 
-					}
+                }
 
-					line = reader.readLine();
-				}
-				reader.close();
+                line = reader.readLine();
+            }
+            reader.close();
 
-			}
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException Caught");
+        }
 
-			catch(NullPointerException e)
-			{
-				System.out.println("NullPointerException Caught");
-			}
-
-		return mapSymptoms;
-	}
+        return mapSymptoms;
+    }
 }
