20e6b28cc6dde23e17b0f5d2d087f87a7ba30031
Jérôme Guéguin <jerome.gueguin@gmail.com>
2021-03-25 09:54:12 +0100

    Final code V2.0 with javadoc comments, code optimisation in main method

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,27 +1,61 @@
 package com.hemebiotech.analytics;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Main program to read a txt file and to print the numbers of symptoms in a file and on console
  *
  * @author jgueguin
- * @version 1.1
+ * @version 2.0
  */
 
 public class AnalyticsCounter {
 
-	// path and file name defined as constants
-		static String pathNameFile = "Project02Eclipse/src/symptoms.txt";
+    // path and file name defined as constants
+    static String pathNameFile = "Project02Eclipse/src/symptoms.txt";
 
-	public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
 
-		ReadSymptomDataFromFile file = new ReadSymptomDataFromFile(pathNameFile);
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+        analyticsCounter.processData();
+    }
 
-		PrintSymptomDataToFile output = new PrintSymptomDataToFile(file.GetSymptoms());
+    // Process data method
+    public void processData() throws IOException {
 
-		output.printFile();
+        ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile(pathNameFile);
+        Map<String, Integer> mapSymptoms = reader.GetSymptoms();
+        this.outputData(mapSymptoms);
+
+    }
+
+    // method to print results in a file and on console
+    public void outputData(Map<String, Integer> mapSymptoms) throws IOException {
+        // output generation
+        FileWriter writer = new FileWriter("result.out");
+
+        System.out.println("Symptoms   :  occurences");
+        writer.write("Symptoms   :  occurences \n");
+
+        mapSymptoms.forEach((name, Integer) -> {
+
+            try {
+                System.out.println(name + ": " + mapSymptoms.get(name));
+                writer.write(name + ":" + mapSymptoms.get(name) + "\n");
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+        });
+
+        writer.close();
+    }
 
 
-	}
 }
+
+
